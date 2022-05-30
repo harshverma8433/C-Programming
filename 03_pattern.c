8261fd206463680b2f9fa932f2916f61148cb12a
harshverma8433 <93326986+harshverma8433@users.noreply.github.com>
2022-05-31 00:11:12 +0530

    Update 03_pattern.c

--- 03_pattern.c
+++ 03_pattern.c
@@ -14,17 +14,3 @@ int main(){
     return 0; 
 
 }
-
-// # include<stdio.h> 
-
-// void main(){
-//     int n;
-//     printf("Enter Any number: ");
-//     scanf("%d",&n);
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<=i;j++){
-//             printf("%d ",j+1);
-//         }
-//         printf("\n");
-//     }
-// }
